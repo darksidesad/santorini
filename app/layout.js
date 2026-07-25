645a3953dc0fcdf0d1a8dba5a55f6d9ff638510b
Johnn <johnn@example.com>
2026-07-24 23:40:34 -0500

    Add Umami analytics script

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import "./globals.css";
+import Script from "next/script";
 import { Montserrat } from "next/font/google";
 import WhatsAppButton from "./components/WhatsAppButton";
 
@@ -25,6 +26,12 @@ export default function RootLayout({ children }) {
       <body>
         {children}
         <WhatsAppButton />
+        <Script
+          defer
+          src="https://umami.luxurycitas.com/script.js"
+          data-website-id="3c272cc7-959b-4806-aac8-0f7c9ddc49e2"
+          strategy="afterInteractive"
+        />
       </body>
     </html>
   );
